ba3f955d44f7839d25a2311450eff258148bded1
Kneidl18 <andreas.auer03@gmail.com>
2023-07-16 14:49:02 +0200

    fixed branch

--- blowfish.h
+++ blowfish.h
@@ -12,17 +12,19 @@
 #define __blowfish__
 
 #include <stdint.h>
+#include <HardwareSerial.h>
 
 class Blowfish {
 public:
-    void SetKey(const unsigned char* key, int byte_length);
-    void Encrypt(unsigned char* dst, const unsigned char* src, int byte_length) const;
-    void Decrypt(unsigned char* dst, const unsigned char* src, int byte_length) const;
+    void SetKey(const  char* key, int byte_length);
+    void Encrypt( char* dst, const  char* src, int byte_length) const;
+    void Decrypt( char* dst, const  char* src, int byte_length) const;
     
 private:
     void EncryptBlock(uint32_t *left, uint32_t *right) const;
     void DecryptBlock(uint32_t *left, uint32_t *right) const;
     uint32_t Feistel(uint32_t value) const;
+    void CheckZero(uint32_t *block) const;
     
 private:
     uint32_t pary_[18];
